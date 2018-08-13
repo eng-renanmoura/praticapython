5638ea4de5e4719aa97ca1158472c923bbbda1b3
Renan Moura <renan.ferreira@trt8.jus.br>
2018-08-13 14:36:04 -0300

    ex2

--- adivinha.py
+++ adivinha.py
@@ -7,7 +7,13 @@ chute = input('Digite o seu numero: ')
 
 print('Voce digitou', chute)
 
-if(numero_secreto == int(chute)):
+acertou = numero_secreto == int(chute)
+maior = int(chute) > numero_secreto
+menor = int(chute) < numero_secreto
+
+if(acertou):
     print('Certo')
+elif(maior):
+    print('Chute acima')
 else:
-    print('Errado')
+    print('Chute abaixo')
